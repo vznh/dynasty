298f15dea86b5cf0d764744981ae518c1560a806
Jason Son <jasonson2004@gmail.com>
2024-10-16 22:10:45 -0700

    Initial commit

---
+++ src/models/fonts.ts
@@ -0,0 +1,8 @@
+// [START fonts.ts]
+import { Quattrocento } from "next/font/google";
+
+export const QuattrocentoHead = Quattrocento({
+  weight: "400",
+  subsets: ["latin"]
+});
+// [END fonts.ts]
